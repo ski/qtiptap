c2f542ee34a13c5df99f9ab341a82ff14fa186c5
Suhail Manzoor <suhailski@gmail.com>
2021-03-09 18:25:49 +0000

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <tiptap />
+  </div>
+</template>
+
+<script>
+import Tiptap from "./Tiptap";
+
+export default {
+  name: "App",
+  components: {
+    Tiptap,
+  },
+};
+</script>
